46e76b85bbda166a26244e7e96b2f19062023288
gauta <gauta@PC2512-7GVEE9PT>
2025-02-12 00:07:55 +0530

    added my sol till chapter 8

--- microbit/src/07-uart/src/main.rs
+++ microbit/src/07-uart/src/main.rs
@@ -2,8 +2,10 @@
 #![no_std]
 
 use cortex_m_rt::entry;
-use rtt_target::rtt_init_print;
+use rtt_target::{rtt_init_print,rprintln};
 use panic_rtt_target as _;
+use core::fmt::Write;
+use heapless::Vec;
 
 #[cfg(feature = "v1")]
 use microbit::{
@@ -39,6 +41,7 @@ fn main() -> ! {
         )
     };
 
+
     #[cfg(feature = "v2")]
     let mut serial = {
         let serial = uarte::Uarte::new(
@@ -50,8 +53,56 @@ fn main() -> ! {
         UartePort::new(serial)
     };
 
-    nb::block!(serial.write(b'X')).unwrap();
-    nb::block!(serial.flush()).unwrap();
+    // nb::block!(serial.write(b'X')).unwrap();
+    // nb::block!(serial.flush()).unwrap();
 
-    loop {}
+    // let sen: &str = "The quick brown fox jumps over the lazy dog \n";
+    // for char in sen.as_bytes(){
+    //     nb::block!(serial.write(*char)).unwrap();
+    // }
+    // nb::block!(serial.flush()).unwrap();
+
+    let mut buffer: Vec<u8,32> = Vec::new();
+
+    // My solution
+    // loop {
+    //     // let byte: u8 = nb::block!(serial.read()).unwrap();
+        
+    //     // rprintln!("{}",byte);
+    //     // nb::block!(serial.write(byte)).unwrap();
+    //     // nb::block!(serial.flush()).unwrap();
+
+    //     let byte: u8 = nb::block!(serial.read()).unwrap();
+    //     // rprintln!("{}",byte);
+    //     if byte == 13u8 {
+    //         buffer.reverse();
+    //         for char in &buffer {
+    //             nb::block!(serial.write(*char)).unwrap();
+    //             nb::block!(serial.flush()).unwrap();
+    //         }
+    //         buffer.clear();
+    //     }
+    //     else {
+    //         buffer.push(byte);
+    //     }
+    // }
+
+    loop {
+        buffer.clear();
+
+        loop {
+            let byte: u8 = nb::block!(serial.read()).unwrap();
+            if buffer.push(byte).is_err() {
+                write!(serial,"Buffer is full").unwrap();
+                break;
+            }
+
+            if byte == 13 {
+                for char in buffer.iter().rev().chain(&[b'\n', b'\r']) {
+                    nb::block!(serial.write(*char)).unwrap();
+                }
+                break;
+            } 
+        }
+    }
 }
--- microbit/src/08-i2c/src/main.rs
+++ microbit/src/08-i2c/src/main.rs
@@ -1,12 +1,13 @@
-#![deny(unsafe_code)]
+// #![deny(unsafe_code)]
 #![no_main]
 #![no_std]
 
 use cortex_m_rt::entry;
 use rtt_target::{rtt_init_print, rprintln};
 use panic_rtt_target as _;
-
-use microbit::hal::prelude::*;
+use core::str;
+use core::fmt::Write;
+use heapless::Vec;
 
 #[cfg(feature = "v1")]
 use microbit::{
@@ -17,36 +18,110 @@ use microbit::{
 #[cfg(feature = "v2")]
 use microbit::{
     hal::twim,
+    hal::prelude::*,
+    hal::uarte,
+    hal::uarte::{Baudrate, Parity},
     pac::twim0::frequency::FREQUENCY_A,
 };
 
-const ACCELEROMETER_ADDR: u8 = 0b0011001;
-const MAGNETOMETER_ADDR: u8 = 0b0011110;
+#[cfg(feature= "v2")]
+mod serial_setup;
+#[cfg(feature = "v2")]
+use serial_setup::UartePort;
 
-const ACCELEROMETER_ID_REG: u8 = 0x0f;
-const MAGNETOMETER_ID_REG: u8 = 0x4f;
+use lsm303agr::{
+    MagOutputDataRate, AccelOutputDataRate, Lsm303agr,
+};
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
     let board = microbit::Board::take().unwrap();
 
+    #[cfg(feature = "v2")]
+    let mut serial = {
+        let serial = uarte::Uarte::new(
+            board.UARTE0,
+            board.uart.into(),
+            Parity::EXCLUDED,
+            Baudrate::BAUD115200,
+        );
+        UartePort::new(serial)
+    };
+
 
     #[cfg(feature = "v1")]
-    let mut i2c = { twi::Twi::new(board.TWI0, board.i2c.into(), FREQUENCY_A::K100) };
+    let i2c = { twi::Twi::new(board.TWI0, board.i2c.into(), FREQUENCY_A::K100) };
 
     #[cfg(feature = "v2")]
-    let mut i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
+    let i2c = { twim::Twim::new(board.TWIM0, board.i2c_internal.into(), FREQUENCY_A::K100) };
 
-    let mut acc = [0];
-    let mut mag = [0];
+    // Code from documentation
+    let mut sensor = Lsm303agr::new_with_i2c(i2c);
+    sensor.init().unwrap();
+    sensor.set_accel_odr(AccelOutputDataRate::Hz10).unwrap();
+    sensor.set_mag_odr(MagOutputDataRate::Hz10).unwrap();
+    let mut sensor = sensor.into_mag_continuous().ok().unwrap();
 
-    // First write the address + register onto the bus, then read the chip's responses
-    i2c.write_read(ACCELEROMETER_ADDR, &[ACCELEROMETER_ID_REG], &mut acc).unwrap();
-    i2c.write_read(MAGNETOMETER_ADDR, &[MAGNETOMETER_ID_REG], &mut mag).unwrap();
+    let mut buffer:Vec<u8,32> = Vec::new();
+    let mut sensor_mode:u8 = 0;
 
-    rprintln!("The accelerometer chip's id is: {:#b}", acc[0]);
-    rprintln!("The magnetometer chip's id is: {:#b}", mag[0]);
+    loop {
+        buffer.clear();
 
-    loop {}
+        loop {
+
+            let byte: u8 = nb::block!(serial.read()).unwrap();
+            rprintln!("{}",byte as char);
+            nb::block!(serial.write(byte)).unwrap();
+            nb::block!(serial.flush()).unwrap();
+            if buffer.push(byte).is_err() {
+                write!(serial,"buffer is full").unwrap();
+                break;
+            }
+
+            if byte == 13 {
+                let comand:&str = str::from_utf8(&buffer).unwrap().trim();
+                if comand == "magnetometer" {
+                    sensor_mode = 1;
+                    buffer.clear();
+                }
+                else if comand == "accelerometer" {
+                    sensor_mode  = 2;
+                    buffer.clear();
+                }
+                else {
+                    rprintln!("{}",comand);
+                    write!(serial,"\r\nwrong comand : {} \n\r",comand).unwrap();
+                    nb::block!(serial.flush()).unwrap();
+                    // sensor_mode = 0;
+                    buffer.clear();
+                }
+            }
+            
+            if sensor_mode == 1 {
+                if sensor.mag_status().unwrap().xyz_new_data {
+                    let data = sensor.mag_data().unwrap();
+                    // rprintln!("Mag : x {} y {} z {}", data.x, data.y, data.z);
+                    write!(serial,"Mag reading x:{}, y:{}, z:{}.\r\n",data.x, data.y, data.z).unwrap();
+                    // nb::block!(serial.flush()).unwrap();
+                }
+            }
+            else if sensor_mode == 2 {
+                if sensor.accel_status().unwrap().xyz_new_data {
+                    let data = sensor.accel_data().unwrap();
+                    // rprintln!("Acceleration: x {} y {} z {}", data.x, data.y, data.z);
+                    write!(serial,"Accel reading x:{}, y:{}, z:{}.\r\n",data.x, data.y, data.z).unwrap();
+                    // nb::block!(serial.flush()).unwrap();
+                }
+            }
+            else {
+                // rprintln!("{}",byte as char);
+                // nb::block!(serial.write(byte)).unwrap();
+                // nb::block!(serial.flush()).unwrap();
+                continue;
+            }
+        }
+    }
 }
+
